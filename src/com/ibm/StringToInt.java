2dbbf961e91d59738eac0513c12d807c909c2f7f
shubhamthonge <shubhamthonge131@gmail.com>
2024-09-25 14:03:30 +0530

    array convert

---
+++ src/com/ibm/StringToInt.java
@@ -0,0 +1,9 @@
+package com.ibm;
+
+public class StringToInt {
+	public static void main(String[] args) {
+		String str = "123";
+		int num = Integer.parseInt(str);
+		System.out.println(num); // Output: 123
+	}
+}
