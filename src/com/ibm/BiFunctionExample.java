a966177e3bab8ea677c4205e191847171ff67ab7
shubhamthonge <shubhamthonge131@gmail.com>
2024-10-10 12:14:05 +0530

    update

--- src/com/ibm/BiFunctionExample.java
+++ src/com/ibm/BiFunctionExample.java
@@ -3,12 +3,12 @@ package com.ibm;
 import java.util.function.BiFunction;
 
 public class BiFunctionExample {
-    public static void main(String[] args) {
-        // BiFunction to add two numbers
-        BiFunction<Integer, Integer, Integer> add = (a, b) -> a + b;
-        
-        // Apply the BiFunction
-        System.out.println(add.apply(10, 20)); // 30
-        System.out.println(add.apply(15, 25)); // 40
-    }
+	public static void main(String[] args) {
+		// BiFunction to add two numbers
+		BiFunction<Integer, Integer, Integer> add = (a, b) -> a + b;
+
+		// Apply the BiFunction
+		System.out.println(add.apply(10, 20)); // 30
+		System.out.println(add.apply(15, 25)); // 40
+	}
 }
