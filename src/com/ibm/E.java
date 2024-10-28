ba01fe8048840f169b347c353df970b0b26c892d
shubhamthonge <shubhamthonge131@gmail.com>
2024-10-28 21:38:06 +0530

    add19

--- src/com/ibm/E.java
+++ src/com/ibm/E.java
@@ -3,9 +3,11 @@ package com.ibm;
 public class E {
 
 	public static void main(String[] args) {
-		int counter = 1; // Initialize the counter to start from 1
-		int rows = 7; // Number of rows
-
+		// Initialize the counter to start from 1
+		int counter = 1; 
+		// Number of rows
+		int rows = 7;
+		
 		for (int i = 1; i <= rows; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(counter + " ");
